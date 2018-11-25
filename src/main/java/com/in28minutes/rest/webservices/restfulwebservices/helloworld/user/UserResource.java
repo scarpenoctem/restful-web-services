1962b19f355631e55235431a4a62274acb9b210a
Dingchingger <dingchingger@hotmail.com>
2018-11-25 17:39:21 -0500

    Initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/user/UserResource.java
@@ -0,0 +1,21 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserResource {
+
+	@Autowired
+	private UserDaoService service;
+
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+	
+}
